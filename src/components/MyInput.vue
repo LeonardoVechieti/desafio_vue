320dc6ed7cf6604d1ffdd52e6c0685945d1de80d
LeonardoVechieti <92764500+LeonardoVechieti@users.noreply.github.com>
2022-06-17 16:40:00 -0300

    create-develope

--- src/components/MyInput.vue
+++ src/components/MyInput.vue
@@ -1,45 +1,46 @@
 <template>
   <div>
-    <form class="form">
-        <div class="input" id="form-1">
-            <label for="name">Full Name *</label>
-            <input type="text" id="name" class="input-1" placeholder="Name">
-            <span id="alertName" class="alert"></span>
-        </div>
-            
-        <div class="input" id="form-2">
-            <label for="email">Email *</label>
-            <input type="email" id="email" class="input-2" placeholder="foo@bar.com">
-            <span id="alertEmail" class="alert"></span>
-        </div>
-        <div class="input" id="form-3">
-            <label for="phone">Phone</label>
-            <input type="tel" id="phone" class="input-3" placeholder="(83) 00000-0000">
-            <span id="alertPhone" class="alert"></span>
-        </div>
-            
-            
-        <div class="input" id="form-4">
-            <label for="password">Password *</label>
-            <input type="password" id="password" class="input-4">
-            <span id="alertPassword" class="alert"></span>
-        </div>
-        <div class="input" id="form-5">
-            <label for="date">Birthday *</label>
-            <input type="date" id="date" class="input-5" placeholder="">
-            <span id="alertDate" class="alert"></span>
-        </div>
-            
-    </form>
-  </div>  
+  <form class="form">
+    <div :class="classDivForm" >
+      <label :for="labelInput">{{ fieldInput }}  </label>
+      <input
+        :type="type"
+        :class="classInput"
+        :placeholder="placeholderInput"
+        :id="idInput"
+        :value="valueInput"
+      />
+    
+  </div>
+  </form>
+  </div>
 </template>
 
-<style scoped>
+<script>
+
+export default {
+  name: "MyInput",
+  components: {},
+  props: {
+    placeholderInput: String,
+    classInput: String,
+    idInput: String,
+    labelInput: String,
+    type: String,
+    classDivForm: String,
+    valueInput: String,
+    fieldInput: String,
+    
+  }
+  
+ }
+</script>
+
+<style scooped>
 .form {
 background-color: #FFFFFF;
 margin-top: 1em;
 margin-bottom: 2em;
-
 border: none;
 border-radius: 15px;
 display: flex;
@@ -60,23 +61,23 @@ flex-direction: row;
     border-radius: 5px;
     outline: none;
 }
-#form-1 {
+.form-1 {
   width: 95%;
 }
-#form-1,
-#form-2 {
+.form-1,
+.form-2 {
   padding-bottom: 15px;
   
 }
 
-#form-2,
-#form-4 {
-  width: 55%;
+.form-2,
+.form-4 {
+  width: 50%;
   
 }
 
-#form-3,
-#form-5 {
+.form-3,
+.form-5 {
   width: 35%;
   padding-left: 2em;
   
@@ -102,13 +103,13 @@ flex-direction: row;
     .input{
       
     }
-  #form-1,
-  #form-2,
-  #form-4 {
+  .form-1,
+  .form-2,
+  .form-4 {
     width: 97%;
   }
-  #form-3,
-  #form-5 {
+  .form-3,
+  .form-5 {
     display: flex;
     flex-flow: column;
     order: +1;
@@ -119,10 +120,10 @@ flex-direction: row;
   }
 
 
-  #form-5 {
+  .form-5 {
     width: 55%;
   }
-    #form-3 {
+    .form-3 {
     
   }
 
@@ -141,5 +142,5 @@ flex-direction: row;
   
 }
 
-</style>>
 
+</style>
--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -1,7 +1,59 @@
 <template>
   <div class="page">
     <MyLogo />
-    <MyInput />
+    
+    <MyInput  placeholderInput="Full name"
+        classDivForm="form-1"
+        type="text"
+        classInput="input-1"
+        labelInput="name"
+        idInput="name"
+        value="fullName"
+        smallClass="classSmall"
+        invalidText="Fullname Invalid"
+        fieldInput="Full Name *"
+        />
+    <MyInput
+        placeholderInput="Email"
+        type="email"
+        classInput="input-2"
+        labelInput="email"
+        idInput="email"
+        valueInput=""
+        classDivForm="form-2"
+        fieldInput="Email *"
+         
+      />
+      <MyInput
+        classDivForm="form-3"
+        placeholderInput="Phone"
+        type="text"
+        classInput="input-3"
+        labelInput="phone"
+        idInput="phone"
+        invalidText="Phone Invalid"
+        fieldInput="Phone *"
+      />
+      <MyInput
+        classDivForm="form-4"
+        placeholderInput="Password"
+        type="password"
+        classInput="input-4"
+        labelInput="password"
+        idInput="password"
+        fieldInput="Password *"
+      />
+      <MyInput
+        classDivForm="form-5"
+        type="date"
+        classInput="input-5"
+        labelInput="birthday"
+        idInput="Birthday"
+        invalidText="Date Invalid"
+        fieldInput="Birthday *"
+      />
+      
+    
     <div class="footer">
       <div class="my-check-box">
         <MyCheckbox />
@@ -34,6 +86,7 @@ export default {
 </script>
 
 <style>
+
 body{
     background-color: #ECEEF2;
     font-family: 'Poppins', sans-serif;
@@ -46,9 +99,9 @@ body{
     background-color: #FFFFFF;
     margin-top: 6em;
     margin-bottom: 6em;
-    max-width: 550px;
-    height: 100%;
-    min-height: 20em;
+    width: 35em;
+    
+    
     padding: 1.5em;
     border: none;
     border-radius: 15px;
